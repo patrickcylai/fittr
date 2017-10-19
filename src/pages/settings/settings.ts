cc8d417648f5b82be4d1f9bfc19b7238cf9088be
shanaye <sapp6815@uni.sydney.edu.au>
2017-10-19 11:34:25 +1100

    Develop (#1)
    * Added a pop up for changing daily step goal, no data validation yet
    
    * changed the data to reflect Ray's dataset, messed around with some colour in the graphs

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html'
 })
+
 export class SettingsPage {
+	public goal: number = 5000;  
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+  	public alertCtrl: AlertController) {
+  }
 
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Daily Step Goal',
+      subTitle: 'Enter a new step goal',
+      inputs: [
+      	{ 
+      		name: 'newGoal',
+      		placeholder: 'Enter a new goal'
+      	}
+      ],
+      buttons: [
+      	{
+      		text: 'Cancel',
+      		role: 'cancel',
+      		handler: () => { 
+      			console.log('Cancel clicked');
+      		}
+      	},
+      	{ 
+      		text: 'Save',
+      		role: 'save',
+      		handler: data => { 
+      			this.goal = data.newGoal;
+      			console.log(data.newGoal);
+      		}
+      	}
+      ]
+    });
+    alert.present();
   }
 
 }
